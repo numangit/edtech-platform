c90400148cbbb1343179239702d9fbc64c26cc48
numangit <abdulnumanjob@gmail.com>
2023-03-31 04:15:42 +0600

    registration and login api added

--- src/features/auth/authApi.js
+++ src/features/auth/authApi.js
@@ -1,28 +1,38 @@
 import { apiSlice } from "../api/apiSlice";
+import { login } from "./authSlice";
 
 export const authApi = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
+
         register: builder.mutation({
             query: (data) => ({
                 url: '/register',
                 method: 'POST',
                 body: data
-            })
+            }),
+            async onQueryStarted(arg, { dispatch, queryFulfilled }) {
+                try {
+                    const { data: result } = await queryFulfilled;
+                    const auth = { accessToken: result.accessToken, user: result.user };
+                    localStorage.setItem("authInfo", JSON.stringify(auth));
+                    dispatch(login(auth));
+                } catch (error) { }
+            }
         }),
+
         login: builder.mutation({
             query: (data) => ({
                 url: '/login',
                 method: 'POST',
                 body: data
             }),
-            async onQueryStarted(data, { dispatch, queryFulfilled }) {
+            async onQueryStarted(arg, { dispatch, queryFulfilled }) {
                 try {
                     const { data: result } = await queryFulfilled;
-                    dispatch()
-
-                } catch (error) {
-
-                }
+                    const auth = { accessToken: result.accessToken, user: result.user };
+                    localStorage.setItem("authInfo", JSON.stringify(auth));
+                    dispatch(login(auth));
+                } catch (error) { }
             }
         })
     })
