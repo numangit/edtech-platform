c2a8e23690da32b3dc26d339fd0a7ccb5e11156c
numangit <abdulnumanjob@gmail.com>
2023-03-31 04:09:39 +0600

    authslice initial state added

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -7,6 +7,7 @@ const initialState = {
 
 const authSlice = createSlice({
     name: "auth",
+    initialState,
     reducers: {
         login: (state, action) => {
             state.accessToken = action.payload.accessToken;
