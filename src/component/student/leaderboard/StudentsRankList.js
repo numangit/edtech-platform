f42e40b1e1f449014df3191689e66224de922b96
numangit <abdulnumanjob@gmail.com>
2023-03-31 04:31:19 +0600

    studentRankList component created

---
+++ src/component/student/leaderboard/StudentsRankList.js
@@ -0,0 +1,72 @@
+import React from 'react';
+
+const StudentsRankList = () => {
+    return (
+        <div class="my-8">
+            <h3 class="text-lg font-bold">Top 20 Result</h3>
+            <table class="text-base w-full border border-slate-600/50 rounded-md my-4">
+                <thead>
+                    <tr class="border-b border-slate-600/50">
+                        <th class="table-th !text-center">Rank</th>
+                        <th class="table-th !text-center">Name</th>
+                        <th class="table-th !text-center">Quiz Mark</th>
+                        <th class="table-th !text-center">Assignment Mark</th>
+                        <th class="table-th !text-center">Total</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr class="border-b border-slate-600/50">
+                        <td class="table-td text-center">4</td>
+                        <td class="table-td text-center">Saad Hasan</td>
+                        <td class="table-td text-center">50</td>
+                        <td class="table-td text-center">50</td>
+                        <td class="table-td text-center">100</td>
+                    </tr>
+
+                    <tr class="border-b border-slate-600/50">
+                        <td class="table-td text-center">4</td>
+                        <td class="table-td text-center">Saad Hasan</td>
+                        <td class="table-td text-center">50</td>
+                        <td class="table-td text-center">50</td>
+                        <td class="table-td text-center">100</td>
+                    </tr>
+
+                    <tr class="border-b border-slate-600/50">
+                        <td class="table-td text-center">4</td>
+                        <td class="table-td text-center">Saad Hasan</td>
+                        <td class="table-td text-center">50</td>
+                        <td class="table-td text-center">50</td>
+                        <td class="table-td text-center">100</td>
+                    </tr>
+
+                    <tr class="border-b border-slate-600/50">
+                        <td class="table-td text-center">4</td>
+                        <td class="table-td text-center">Saad Hasan</td>
+                        <td class="table-td text-center">50</td>
+                        <td class="table-td text-center">50</td>
+                        <td class="table-td text-center">100</td>
+                    </tr>
+
+                    <tr class="border-b border-slate-600/50">
+                        <td class="table-td text-center">4</td>
+                        <td class="table-td text-center">Saad Hasan</td>
+                        <td class="table-td text-center">50</td>
+                        <td class="table-td text-center">50</td>
+                        <td class="table-td text-center">100</td>
+                    </tr>
+
+                    <tr class="border-slate-600/50">
+                        <td class="table-td text-center">4</td>
+                        <td class="table-td text-center">Saad Hasan</td>
+                        <td class="table-td text-center">50</td>
+                        <td class="table-td text-center">50</td>
+                        <td class="table-td text-center">100</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default StudentsRankList;
