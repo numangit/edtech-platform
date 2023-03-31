2340741e3111ed20b0ad502b4b6ca7d8552365f7
numangit <abdulnumanjob@gmail.com>
2023-03-31 22:03:00 +0600

    videos fetched using hooks

--- src/component/student/coursePlayer/Videos.js
+++ src/component/student/coursePlayer/Videos.js
@@ -1,9 +1,24 @@
+import { useGetVideosQuery } from '../../../features/videos/videoApi';
 import Video from './Video';
 
 const Videos = () => {
+
+    //hooks
+    const { data: videos, isLoading, isError, error } = useGetVideosQuery();
+
+    //what to render
+    let content = null;
+
+    if (isLoading) content = <div className="text-center">Loading...</div>;
+    if (!isLoading && isError) content = <div className="text-center"> {error?.data}</div>;
+    if (!isLoading && !isError && videos?.length === 0) content = <div className="text-center">No videos found!</div>;
+    if (!isLoading && !isError && videos?.length > 0) {
+        content = videos.map(video => <Video key={video.id} video={video} />)
+    }
+
     return (
         <div className="col-span-full lg:col-auto max-h-[570px] overflow-y-auto bg-secondary p-4 rounded-md border border-slate-50/10 divide-y divide-slate-600/30">
-            <Video />
+            {content}
         </div>
     );
 };
