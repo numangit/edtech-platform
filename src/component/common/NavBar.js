87ce0d353f3966a9ad51c5c48f707c28c4eb08de
numangit <abdulnumanjob@gmail.com>
2023-03-31 19:36:38 +0600

    dynamic navbar added

--- src/component/common/NavBar.js
+++ src/component/common/NavBar.js
@@ -1,22 +1,46 @@
+import { useSelector } from 'react-redux';
 import logo from '../../assets/image/learningportal.svg';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
+    //checking user role
+    const { user } = useSelector(state => state.auth) || {};
+    const { name, role } = user || {};
+
     return (
         <nav className="shadow-md">
             <div className="max-w-7xl px-5 lg:px-0 mx-auto flex justify-between py-3">
-                <img className="h-10" src={logo} alt="LWS logo" />
+
+                {
+                    role === "admin"
+                        ? <Link to="/admin">
+                            <img className="h-10" src={logo} alt="LWS logo" />
+                        </Link>
+                        : <Link to="/videos/1">
+                            <img className="h-10" src={logo} alt="LWS logo" />
+                        </Link>
+                }
+
                 <div className="flex items-center gap-3">
-                    <a href="./Leaderboard.html">Leaderboard</a>
-                    <h2 className="font-bold">Saad Hasan</h2>
-                    <button
-                        className="flex gap-2 border border-cyan items-center px-4 py-1 rounded-full text-sm transition-all hover:bg-cyan ">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                            stroke="currentColor" className="w-6 h-6">
-                            <path stroke-linecap="round" stroke-linejoin="round"
-                                d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
-                        </svg>
-                        Logout
-                    </button>
+
+                    {
+                        role === "student" && <a href="./Leaderboard.html">Leaderboard</a>
+                    }
+
+                    <h2 className="font-bold">{name}</h2>
+
+                    {
+                        user !== undefined
+                        && <button
+                            className="flex gap-2 border border-cyan items-center px-4 py-1 rounded-full text-sm transition-all hover:bg-cyan ">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                stroke="currentColor" className="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
+                            </svg>
+                            Logout
+                        </button>
+                    }
                 </div>
             </div>
         </nav>
