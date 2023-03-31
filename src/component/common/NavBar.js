a9390b1b78e28392a1e73f93a12a8ae1ebb9c132
numangit <abdulnumanjob@gmail.com>
2023-03-31 20:04:54 +0600

    logout component added

--- src/component/common/NavBar.js
+++ src/component/common/NavBar.js
@@ -2,20 +2,15 @@ import { useDispatch, useSelector } from 'react-redux';
 import logo from '../../assets/image/learningportal.svg';
 import { Link } from 'react-router-dom';
 import { logout } from '../../features/auth/authSlice';
+import LogoutButton from './LogoutButton';
 
 const NavBar = () => {
 
     //hooks
     const { user } = useSelector(state => state.auth) || {};
-    const dispatch = useDispatch();
 
     //checking user role
     const { name, role } = user || {};
-
-    //function to handle logout
-    const handleLogout = () => {
-        dispatch(logout());
-    };
 
     return (
         <nav className="shadow-md">
@@ -40,17 +35,7 @@ const NavBar = () => {
                     <h2 className="font-bold">{name}</h2>
 
                     {
-                        user !== undefined
-                        && <button
-                            onClick={handleLogout}
-                            className="flex gap-2 border border-cyan items-center px-4 py-1 rounded-full text-sm transition-all hover:bg-cyan ">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5"
-                                stroke="currentColor" className="w-6 h-6">
-                                <path strokeLinecap="round" strokeLinejoin="round"
-                                    d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15M12 9l-3 3m0 0l3 3m-3-3h12.75" />
-                            </svg>
-                            Logout
-                        </button>
+                        user !== undefined && <LogoutButton />
                     }
 
                 </div>
