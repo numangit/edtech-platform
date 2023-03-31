da4e7c9a03720e9e3bd3d4a977ee0a63b2095dab
numangit <abdulnumanjob@gmail.com>
2023-03-31 19:06:54 +0600

    admin login added

--- src/pages/admin/AdminLogin.js
+++ src/pages/admin/AdminLogin.js
@@ -1,6 +1,35 @@
+import { useState } from 'react';
 import logo from '../../assets/image/learningportal.svg';
+import { useLoginMutation } from '../../features/auth/authApi';
+import { useNavigate } from 'react-router-dom';
 
 const AdminLogin = () => {
+
+    //hooks
+    const [login, { isLoading, isError, error }] = useLoginMutation();
+    const navigate = useNavigate();
+
+    //input states
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    //function to reset
+    const reset = () => {
+        setEmail('');
+        setPassword('');
+    };
+
+    //function to handle form submit
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const data = { email, password };
+        login(data);
+        reset();
+        if (!isError) {
+            navigate('/admin');
+        }
+    };
+
     return (
         <section className="py-6 bg-primary h-screen grid place-items-center">
             <div className="mx-auto max-w-md px-5 lg:px-0">
@@ -10,21 +39,42 @@ const AdminLogin = () => {
                         Sign in to Admin Account
                     </h2>
                 </div>
-                <form className="mt-8 space-y-6" action="#" method="POST">
+                <form onSubmit={handleSubmit} className="mt-8 space-y-6" action="#" method="POST">
                     <input type="hidden" name="remember" value="true" />
                     <div className="rounded-md shadow-sm -space-y-px">
                         <div>
                             <label htmlFor="email-address" className="sr-only">Email address</label>
-                            <input id="email-address" name="email" type="email" autoComplete="email" required
-                                className="login-input rounded-t-md" placeholder="Email address" />
+                            <input
+                                id="email-address"
+                                name="email"
+                                type="email"
+                                autoComplete="email"
+                                required
+                                className="login-input rounded-t-md"
+                                placeholder="Email address"
+                                value={email}
+                                onChange={e => setEmail(e.target.value)} />
                         </div>
                         <div>
                             <label htmlFor="password" className="sr-only">Password</label>
-                            <input id="password" name="password" type="password" autoComplete="current-password" required
-                                className="login-input rounded-b-md" placeholder="Password" />
+                            <input
+                                id="password"
+                                name="password"
+                                type="password"
+                                autoComplete="current-password"
+                                required
+                                className="login-input rounded-b-md"
+                                placeholder="Password"
+                                value={password}
+                                onChange={e => setPassword(e.target.value)} />
                         </div>
                     </div>
-
+                    {
+                        isError &&
+                        <div >
+                            <p className="text-red-500 text-center">{error.data}</p>
+                        </div>
+                    }
                     <div className="flex items-center justify-end">
                         <div className="text-sm">
                             <a href="#" className="font-medium text-violet-600 hover:text-violet-500">
@@ -34,7 +84,7 @@ const AdminLogin = () => {
                     </div>
 
                     <div>
-                        <button type="submit"
+                        <button disabled={isLoading} type="submit"
                             className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-violet-600 hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500">
                             Sign in
                         </button>
