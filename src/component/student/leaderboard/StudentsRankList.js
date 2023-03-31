bd4905b77ce471d7318c29a06027e750a3dd1d0e
numangit <abdulnumanjob@gmail.com>
2023-04-01 04:11:30 +0600

    usersApi added

--- src/component/student/leaderboard/StudentsRankList.js
+++ src/component/student/leaderboard/StudentsRankList.js
@@ -1,6 +1,24 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectAuth } from '../../../features/auth/authSelector';
 
 const StudentsRankList = () => {
+
+    //getting info related to current student
+    const { user } = useSelector(selectAuth) || {};
+    const { id, name } = user || {};
+
+    //getting student quiz mark
+    const { data: quizMarks, isLoading: isQuizLoading } = useGetQuizMarkQuery(id) || {};
+    const totalQuizMark = quizMarks?.reduce((total, current) => total + current.mark, 0);
+
+    //getting student assignment mark
+    const { data: assignmentMarks, isLoading: isAssignmentLoading } = useGetAssignmentMarkQuery(id) || {};
+    const totalAssignmentMark = assignmentMarks?.reduce((total, current) => total + current.mark, 0);
+
+    //calculate grand total
+    const grandTotal = totalQuizMark || 0 + totalAssignmentMark || 0
+
     return (
         <div className="my-8">
             <h3 className="text-lg font-bold">Top 20 Result</h3>
@@ -17,46 +35,6 @@ const StudentsRankList = () => {
 
                 <tbody>
                     <tr className="border-b border-slate-600/50">
-                        <td className="table-td text-center">4</td>
-                        <td className="table-td text-center">Saad Hasan</td>
-                        <td className="table-td text-center">50</td>
-                        <td className="table-td text-center">50</td>
-                        <td className="table-td text-center">100</td>
-                    </tr>
-
-                    <tr className="border-b border-slate-600/50">
-                        <td className="table-td text-center">4</td>
-                        <td className="table-td text-center">Saad Hasan</td>
-                        <td className="table-td text-center">50</td>
-                        <td className="table-td text-center">50</td>
-                        <td className="table-td text-center">100</td>
-                    </tr>
-
-                    <tr className="border-b border-slate-600/50">
-                        <td className="table-td text-center">4</td>
-                        <td className="table-td text-center">Saad Hasan</td>
-                        <td className="table-td text-center">50</td>
-                        <td className="table-td text-center">50</td>
-                        <td className="table-td text-center">100</td>
-                    </tr>
-
-                    <tr className="border-b border-slate-600/50">
-                        <td className="table-td text-center">4</td>
-                        <td className="table-td text-center">Saad Hasan</td>
-                        <td className="table-td text-center">50</td>
-                        <td className="table-td text-center">50</td>
-                        <td className="table-td text-center">100</td>
-                    </tr>
-
-                    <tr className="border-b border-slate-600/50">
-                        <td className="table-td text-center">4</td>
-                        <td className="table-td text-center">Saad Hasan</td>
-                        <td className="table-td text-center">50</td>
-                        <td className="table-td text-center">50</td>
-                        <td className="table-td text-center">100</td>
-                    </tr>
-
-                    <tr className="border-slate-600/50">
                         <td className="table-td text-center">4</td>
                         <td className="table-td text-center">Saad Hasan</td>
                         <td className="table-td text-center">50</td>
---
+++ src/features/users/usersApi.js
@@ -0,0 +1,14 @@
+import { apiSlice } from "../api/apiSlice";
+
+export const userApi = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+
+        getUsers: builder.query({
+            query: () => ({ url: '/users' })
+        }),
+    })
+})
+
+export const {
+    useGetUsersQuery
+} = userApi;
