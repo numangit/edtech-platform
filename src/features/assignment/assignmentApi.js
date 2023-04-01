551055f99f265d087c8528a5facac407b01d61a2
numangit <abdulnumanjob@gmail.com>
2023-04-02 05:59:27 +0600

    get assignments api added

--- src/features/assignment/assignmentApi.js
+++ src/features/assignment/assignmentApi.js
@@ -3,6 +3,10 @@ import { apiSlice } from "../api/apiSlice";
 export const assignmentApi = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
 
+        getAssignments: builder.query({
+            query: () => ({ url: '/assignments' })
+        }),
+
         getAssignmentByVideoId: builder.query({
             query: (id) => ({ url: `/assignments?video_id_like=${id}` })
         }),
