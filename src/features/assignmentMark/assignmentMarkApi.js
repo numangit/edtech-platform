1c7f06c876be5af4b7575a91fd286bea588de253
numangit <abdulnumanjob@gmail.com>
2023-04-02 02:56:47 +0600

    edit assignment mark api added

--- src/features/assignmentMark/assignmentMarkApi.js
+++ src/features/assignmentMark/assignmentMarkApi.js
@@ -2,16 +2,27 @@ import { apiSlice } from "../api/apiSlice";
 
 export const assignmentMarkApi = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
+
         getAssignmentMarks: builder.query({
             query: () => ({ url: '/assignmentMark' })
         }),
+
         getAssignmentMark: builder.query({
             query: (id) => ({ url: `/assignmentMark?student_id_like=${id}` })
+        }),
+
+        editAssignmentMark: builder.mutation({
+            query: ({ id, data }) => ({
+                url: `/assignmentMark/${id}`,
+                method: 'PATCH',
+                body: data,
+            }),
         }),
     })
 })
 
 export const {
     useGetAssignmentMarkQuery,
-    useGetAssignmentMarksQuery
+    useGetAssignmentMarksQuery,
+    useEditAssignmentMarkMutation
 } = assignmentMarkApi;
