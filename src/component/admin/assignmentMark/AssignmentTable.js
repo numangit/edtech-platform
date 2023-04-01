cf53b673963189dcf666c04a4361b5ac84a2a1ae
numangit <abdulnumanjob@gmail.com>
2023-04-02 03:25:44 +0600

    assignment mark row component added

--- src/component/admin/assignmentMark/AssignmentTable.js
+++ src/component/admin/assignmentMark/AssignmentTable.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import AssignmentMarkRow from './AssignmentMarkRow';
 
 const AssignmentTable = () => {
     return (
@@ -14,21 +15,7 @@ const AssignmentTable = () => {
             </thead>
 
             <tbody className="divide-y divide-slate-600/50">
-                <tr>
-                    <td className="table-td">Assignment 1 - Implement Debounce Function</td>
-                    <td className="table-td">10 Mar 2023 10:58:13 PM</td>
-                    <td className="table-td">Saad Hasan</td>
-                    <td className="table-td">https://github.com/Learn-with-Sumit/assignment-1</td>
-                    <td className="table-td input-mark">
-                        <input max="100" value="100" />
-                        <svg fill="none" viewBox="0 0 24 24" strokeWidth="2" stroke="currentColor"
-                            className="w-6 h-6 text-green-500 cursor-pointer hover:text-green-400">
-                            <path strokeLinecap="round" strokeLinejoin="round"
-                                d="M4.5 12.75l6 6 9-13.5" />
-                        </svg>
-
-                    </td>
-                </tr>
+                <AssignmentMarkRow />
                 <tr>
                     <td className="table-td">Assignment 2 - Implement Best Practices</td>
                     <td className="table-td">10 Mar 2023 10:58:13 PM</td>
@@ -43,7 +30,6 @@ const AssignmentTable = () => {
                     <td className="table-td">https://github.com/Learn-with-Sumit/assignment-1</td>
                     <td className="table-td">100</td>
                 </tr>
-
                 <tr>
                     <td className="table-td">Assignment 1 - Scoreboard Application</td>
                     <td className="table-td">10 Mar 2023 10:58:13 PM</td>
