9f512b078b6633308795a302635756402483b105
numangit <abdulnumanjob@gmail.com>
2023-04-02 05:56:15 +0600

    assignment statstics arendered

--- src/component/admin/assignmentMark/Status.js
+++ src/component/admin/assignmentMark/Status.js
@@ -1,12 +1,34 @@
-import React from 'react';
+import { useGetAssignmentMarksQuery } from '../../../features/assignmentMark/assignmentMarkApi';
 
 const Status = () => {
+
+    //getting the assignment marks
+    const { data: marks, isLoading, isError, error } = useGetAssignmentMarksQuery() || {};
+
+    //data to render
+    const total = marks?.length;
+    const pending = marks?.filter(mark => mark.status === "pending").length;
+    const markSent = marks?.filter(mark => mark.status === "published").length;
+
     return (
-        <ul className="assignment-status">
-            <li>Total <span>4</span></li>
-            <li>Pending <span>3</span></li>
-            <li>Mark Sent <span>1</span></li>
-        </ul>
+        <>
+            {
+                (isLoading)
+                && <div><p className='text-center'>Loading...</p></div>
+            }
+            {
+                (!isLoading && isError)
+                && <div><p className='text-center'>{error?.error}</p></div>
+            }
+            {
+                (!isLoading && !isError && marks?.length > 0)
+                && <ul className="assignment-status">
+                    <li>Total <span>{total}</span></li>
+                    <li>Pending <span>{pending}</span></li>
+                    <li>Mark Sent <span>{markSent}</span></li>
+                </ul>
+            }
+        </>
     );
 };
 
