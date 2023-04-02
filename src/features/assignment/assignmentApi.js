23b98f33bb4b470a26bc97e14cb6e362a47696dc
numangit <abdulnumanjob@gmail.com>
2023-04-02 06:33:51 +0600

    delete assignment api added

--- src/features/assignment/assignmentApi.js
+++ src/features/assignment/assignmentApi.js
@@ -10,10 +10,31 @@ export const assignmentApi = apiSlice.injectEndpoints({
         getAssignmentByVideoId: builder.query({
             query: (id) => ({ url: `/assignments?video_id_like=${id}` })
         }),
+
+        deleteAssignment: builder.mutation({
+
+            query: (id) => ({
+                url: `/assignments/${id}`,
+                method: 'DELETE',
+            }),
+
+            // delete optimistically
+            async onQueryStarted(id, { dispatch, queryFulfilled }) {
+                const patchResult = dispatch(apiSlice.util.updateQueryData('getAssignments', undefined, (draft) => {
+                    return draft?.filter((assignment) => assignment?.id !== parseInt(id));
+                }));
+                try {
+                    await queryFulfilled;
+                } catch (err) {
+                    patchResult.undo();
+                };
+            },
+        }),
     })
 })
 
 export const {
     useGetAssignmentsQuery,
-    useGetAssignmentByVideoIdQuery
+    useGetAssignmentByVideoIdQuery,
+    useDeleteAssignmentMutation
 } = assignmentApi;
