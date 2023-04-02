15a6f03f132c85c4b44f928d27e31b40c087d0a1
numangit <abdulnumanjob@gmail.com>
2023-04-02 06:43:15 +0600

    delete quiz query added

--- src/features/quiz/quizApi.js
+++ src/features/quiz/quizApi.js
@@ -3,12 +3,38 @@ import { apiSlice } from "../api/apiSlice";
 export const quizApi = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
 
+        getQuizzes: builder.query({
+            query: () => ({ url: '/quizzes' })
+        }),
+
         getQuizByVideoId: builder.query({
             query: (id) => ({ url: `/quizzes?video_id_like=${id}` })
+        }),
+
+        deleteQuiz: builder.mutation({
+
+            query: (id) => ({
+                url: `/quizzes/${id}`,
+                method: 'DELETE',
+            }),
+
+            // delete optimistically
+            async onQueryStarted(id, { dispatch, queryFulfilled }) {
+                const patchResult = dispatch(apiSlice.util.updateQueryData('getQuizzes', undefined, (draft) => {
+                    return draft?.filter((quiz) => quiz?.id !== parseInt(id));
+                }));
+                try {
+                    await queryFulfilled;
+                } catch (err) {
+                    patchResult.undo();
+                };
+            },
         }),
     })
 })
 
 export const {
-    useGetQuizByVideoIdQuery
+    useGetQuizzesQuery,
+    useGetQuizByVideoIdQuery,
+    useDeleteQuizMutation
 } = quizApi;
