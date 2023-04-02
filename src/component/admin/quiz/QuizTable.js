7a4c45f7c87a43d7acd0a3cfbbbe754a36b40025
numangit <abdulnumanjob@gmail.com>
2023-04-02 06:49:21 +0600

    quizzes rendered

--- src/component/admin/quiz/QuizTable.js
+++ src/component/admin/quiz/QuizTable.js
@@ -1,6 +1,24 @@
+import { useGetQuizzesQuery } from "../../../features/quiz/quizApi";
 import QuizRow from "./QuizRow";
 
 const QuizTable = () => {
+
+    //getting the quizzes
+    const { data: quizzes, isLoading, isError, error } = useGetQuizzesQuery() || {};
+
+    //what to render
+    let content = null;
+
+    if (isLoading) {
+        content = <tr><td className="text-center">Loading...</td></tr>;
+    } else if (!isLoading && isError) {
+        content = <tr><td className="text-center"> {error?.error}</td></tr>;
+    } else if (!isLoading && !isError && quizzes?.length === 0) {
+        content = <tr><td className="text-center">No quizzes found!</td></tr>;
+    } else if (!isLoading && !isError && quizzes?.length > 0) {
+        content = quizzes.map(quiz => <QuizRow key={quiz.id} quiz={quiz} />)
+    };
+
     return (
         <div className="overflow-x-auto mt-4">
             <table className="divide-y-1 text-base divide-gray-600 w-full">
@@ -13,7 +31,7 @@ const QuizTable = () => {
                 </thead>
 
                 <tbody className="divide-y divide-slate-600/50">
-                    <QuizRow />
+                    {content}
                 </tbody>
             </table>
         </div>
