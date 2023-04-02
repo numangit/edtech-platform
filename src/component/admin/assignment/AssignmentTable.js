de466116b89edfb1313c979e2453cd68214a4a05
numangit <abdulnumanjob@gmail.com>
2023-04-02 06:16:26 +0600

    assignments rendered

--- src/component/admin/assignment/AssignmentTable.js
+++ src/component/admin/assignment/AssignmentTable.js
@@ -1,6 +1,24 @@
+import { useGetAssignmentsQuery } from '../../../features/assignment/assignmentApi';
 import AssignmentRow from './AssignmentRow';
 
 const AssignmentTable = () => {
+
+    //getting the assignments
+    const { data: assignments, isLoading, isError, error } = useGetAssignmentsQuery() || {};
+
+    //what to render
+    let content = null;
+
+    if (isLoading) {
+        content = <tr><td className="text-center">Loading...</td></tr>;
+    } else if (!isLoading && isError) {
+        content = <tr><td className="text-center"> {error?.error}</td></tr>;
+    } else if (!isLoading && !isError && assignments?.length === 0) {
+        content = <tr><td className="text-center">No assignments found!</td></tr>;
+    } else if (!isLoading && !isError && assignments?.length > 0) {
+        content = assignments.map(assignment => <AssignmentRow key={assignment.id} assignment={assignment} />)
+    };
+
     return (
         <div className="overflow-x-auto mt-4">
             <table className="divide-y-1 text-base divide-gray-600 w-full">
@@ -14,7 +32,7 @@ const AssignmentTable = () => {
                 </thead>
 
                 <tbody className="divide-y divide-slate-600/50">
-                    <AssignmentRow />
+                    {content}
                 </tbody>
             </table>
         </div>
