bb59697aa6612dc521de39d1917ffca0fb1ab609
numangit <abdulnumanjob@gmail.com>
2023-04-03 23:18:38 +0600

    addVideo component added

---
+++ src/component/admin/adminVideo/AddVideo.js
@@ -0,0 +1,9 @@
+const AddVideo = () => {
+    return (
+        <div className="w-full flex">
+            <button className="btn ml-auto">Add Video</button>
+        </div>
+    );
+};
+
+export default AddVideo;
--- src/pages/admin/AdminVideos.js
+++ src/pages/admin/AdminVideos.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import VideoTable from '../../component/admin/adminVideo/VideoTable';
+import AddVideo from '../../component/admin/adminVideo/AddVideo';
 
 const AdminVideos = () => {
     return (
         <section className="py-6 bg-primary">
             <div className="mx-auto max-w-full px-5 lg:px-20">
                 <div className="px-3 py-20 bg-opacity-10">
-                    <div className="w-full flex">
-                        <button className="btn ml-auto">Add Video</button>
-                    </div>
+                    <AddVideo />
                     <div className="overflow-x-auto mt-4">
                         <VideoTable />
                     </div>
