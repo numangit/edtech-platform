3d5c0234a82c3031197fa513116b19336d588a90
numangit <abdulnumanjob@gmail.com>
2023-04-04 01:14:40 +0600

    quizHeader component added

---
+++ src/component/student/quizzes/QuizHeader.js
@@ -0,0 +1,25 @@
+import { useParams } from "react-router-dom";
+import { useGetVideoQuery } from "../../../features/videos/videoApi";
+
+const QuizHeader = () => {
+
+    //getting video id from url
+    const { id } = useParams();
+
+    //getting video data
+    const { data: video, isLoading } = useGetVideoQuery(id) || {};
+    const { title } = video || {};
+    return (
+        <>
+            {
+                isLoading &&
+                <div className="mb-8">
+                    <h1 className="text-2xl font-bold">Quizzes for "{title}"</h1>
+                    <p className="text-sm text-slate-200">Each question contains 5 Mark</p>
+                </div>
+            }
+        </>
+    );
+};
+
+export default QuizHeader;
--- src/pages/student/Quizzes.js
+++ src/pages/student/Quizzes.js
@@ -1,27 +1,11 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import { useGetVideoQuery } from '../../features/videos/videoApi';
 import QuizForm from '../../component/student/quizzes/QuizForm';
+import QuizHeader from '../../component/student/quizzes/QuizHeader';
 
 const Quizzes = () => {
-
-    //getting video id from url
-    const { id } = useParams();
-
-    //getting video data
-    const { data: video, isLoading: isTitleLoading } = useGetVideoQuery(id) || {};
-    const { title } = video || {};
-
     return (
         <section className="py-6 bg-primary">
             <div className="mx-auto max-w-7xl px-5 lg:px-0">
-                {
-                    !isTitleLoading
-                    && <div className="mb-8">
-                        <h1 className="text-2xl font-bold">Quizzes for "{title}"</h1>
-                        <p className="text-sm text-slate-200">Each question contains 5 Mark</p>
-                    </div>
-                }
+                <QuizHeader />
 
                 <div className="space-y-8 ">
                     <QuizForm />
