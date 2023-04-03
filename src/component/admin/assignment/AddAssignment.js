4057fcbdabd70c6494d2eb2cdcccc5baa62908eb
numangit <abdulnumanjob@gmail.com>
2023-04-04 02:14:46 +0600

    addAssignment component added

---
+++ src/component/admin/assignment/AddAssignment.js
@@ -0,0 +1,9 @@
+const AddAssignment = () => {
+    return (
+        <div className="w-full flex">
+            <button className="btn ml-auto">Add Assignment</button>
+        </div>
+    );
+};
+
+export default AddAssignment;
--- src/pages/admin/Assignment.js
+++ src/pages/admin/Assignment.js
@@ -1,3 +1,4 @@
+import AddAssignment from "../../component/admin/assignment/AddAssignment";
 import AssignmentTable from "../../component/admin/assignment/AssignmentTable";
 
 const Assignment = () => {
@@ -5,9 +6,7 @@ const Assignment = () => {
         <section className="py-6 bg-primary">
             <div className="mx-auto max-w-full px-5 lg:px-20">
                 <div className="px-3 py-20 bg-opacity-10">
-                    <div className="w-full flex">
-                        <button className="btn ml-auto">Add Assignment</button>
-                    </div>
+                    <AddAssignment />
                     <AssignmentTable />
                 </div>
             </div>
