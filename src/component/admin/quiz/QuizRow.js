30c7728774bd863dbaf4944ca5f7b51199a00760
numangit <abdulnumanjob@gmail.com>
2023-04-04 20:31:12 +0600

    edit quiz component added

--- src/component/admin/quiz/QuizRow.js
+++ src/component/admin/quiz/QuizRow.js
@@ -1,5 +1,6 @@
 import { useDeleteQuizMutation } from "../../../features/quiz/quizApi";
 import DeleteButton from "../../common/DeleteButton";
+import EditQuiz from "./EditQuiz";
 
 const QuizRow = ({ quiz }) => {
 
@@ -25,14 +26,9 @@ const QuizRow = ({ quiz }) => {
                         : video_title
                 }
             </td>
-            <td className="table-td flex gap-x-2 justify-center">
+            <td className="flex gap-x-2 justify-center text-gray-500">
                 <DeleteButton id={id} mutation={deleteQuiz} />
-                <svg fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor"
-                    className="w-6 h-6 hover:text-blue-500 cursor-pointer transition-all">
-                    <path strokeLinecap="round" strokeLinejoin="round"
-                        d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
-                </svg>
-
+                <EditQuiz id={id} />
             </td>
         </tr>
     );
