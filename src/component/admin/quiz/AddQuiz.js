cfe60bd5a4d1b95584bfc82fc3e640ce53b053d3
numangit <abdulnumanjob@gmail.com>
2023-04-04 06:13:24 +0600

    addQuiz component added

---
+++ src/component/admin/quiz/AddQuiz.js
@@ -0,0 +1,9 @@
+const AddQuiz = () => {
+    return (
+        <div className="w-full flex">
+            <button className="btn ml-auto">Add Quiz</button>
+        </div>
+    );
+};
+
+export default AddQuiz;
--- src/pages/admin/AdminQuiz.js
+++ src/pages/admin/AdminQuiz.js
@@ -1,3 +1,4 @@
+import AddQuiz from "../../component/admin/quiz/AddQuiz";
 import QuizTable from "../../component/admin/quiz/QuizTable";
 
 const AdminQuiz = () => {
@@ -5,9 +6,7 @@ const AdminQuiz = () => {
         <section className="py-6 bg-primary">
             <div className="mx-auto max-w-full px-5 lg:px-20">
                 <div className="px-3 py-20 bg-opacity-10">
-                    <div className="w-full flex">
-                        <button className="btn ml-auto">Add Quiz</button>
-                    </div>
+                    <AddQuiz />
                     <QuizTable />
                 </div>
             </div>
