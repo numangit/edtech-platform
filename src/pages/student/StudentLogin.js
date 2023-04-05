6c1463dc7d011a9d30c8843b63d137e82b27b791
numangit <abdulnumanjob@gmail.com>
2023-04-05 07:30:10 +0600

    login page button changed

--- src/pages/student/StudentLogin.js
+++ src/pages/student/StudentLogin.js
@@ -116,7 +116,7 @@ const StudentLogin = () => {
                     <Link to="/admin/login">
                         <button disabled={isLoading} type="submit"
                             className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-slate-900 hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500 my-3">
-                            Admin Login
+                            Admin Login Page
                         </button>
                     </Link>
                 </form>
